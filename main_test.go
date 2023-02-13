8907bcf8758fdadc524ef15afe535039ad21957d
Michael Anstis <manstis@redhat.com>
2023-02-13 14:34:25 +0000

    Duplicate records

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 )
 
@@ -9,12 +11,42 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	obj := Person{
+		Address: Address{
+			AddressType: "home",
+			AddressLines: []AddressLine{{
+				Text: "Line1",
+			},
+				{
+					Text: "Line2",
+				}},
+		},
+	}
 
-	DB.Create(&user)
+	DB.Debug().Create(&obj)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
+	var result People
+	dbConn := DB.Table("people")
+	dbConn = dbConn.Select(
+		"people.*",
+		"a.*",
+		"al.*",
+	)
+	dbConn = dbConn.Joins("LEFT JOIN addresses a ON a.id = people.address_id")
+	dbConn = dbConn.Joins("LEFT JOIN address_lines al ON al.address_id = a.id")
+	dbConn.Preload("Address")
+	dbConn.Preload("Address.AddressLines")
+
+	if err := dbConn.Debug().Find(&result, obj.ID).Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
+
+	fmt.Println("Resultset size: ", len(result))
+
+	b, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
 }

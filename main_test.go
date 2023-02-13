dcaba4da44252cec9bf38021df96ba579ae0d55c
Michael Anstis <manstis@redhat.com>
2023-02-13 19:09:44 +0000

    Unscoped on joins

--- main_test.go
+++ main_test.go
@@ -23,30 +23,42 @@ func TestGORM(t *testing.T) {
 		},
 	}
 
-	DB.Debug().Create(&obj)
+	DB.Create(&obj)
 
 	var result People
-	dbConn := DB.Table("people")
-	dbConn = dbConn.Select(
-		"people.*",
-		"a.*",
-		"al.*",
-	)
-	dbConn = dbConn.Joins("LEFT JOIN addresses a ON a.id = people.address_id")
-	dbConn = dbConn.Joins("LEFT JOIN address_lines al ON al.address_id = a.id")
-	dbConn.Preload("Address")
-	dbConn.Preload("Address.AddressLines")
-
-	if err := dbConn.Debug().Find(&result, obj.ID).Error; err != nil {
+	if err := DB.Preload("Address.AddressLines").Find(&result).Joins("Address").Joins("AddressLine").Error; err != nil {
 		t.Errorf("Failed, got error: %v", err)
 	}
 
+	fmt.Println("=STORE====================")
 	fmt.Println("Resultset size: ", len(result))
-
-	b, err := json.MarshalIndent(result, "", "  ")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	b, _ := json.MarshalIndent(result, "", "  ")
 	fmt.Println(string(b))
+
+	if err := DB.Delete(&result[0].Address).Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+
+	fmt.Println("=UNSCOPED LOAD====================")
+	var result2 People
+	if err := DB.Unscoped().Preload("Address.AddressLines").Find(&result2).Joins("Address").Joins("AddressLine").Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	b2, _ := json.MarshalIndent(result2, "", "  ")
+	fmt.Println(string(b2))
+
+	fmt.Println("=SCOPED LOAD====================")
+	var result3 People
+	if err := DB.Preload("Address.AddressLines").Find(&result3).Joins("Address").Joins("AddressLine").Error; err != nil {
+		t.Errorf("Failed, got error: %v", err)
+	}
+	b3, _ := json.MarshalIndent(result3, "", "  ")
+	fmt.Println(string(b3))
+
+	fmt.Println("=ADDRESSES====================")
+	address := &Address{}
+	DB.Model(&Address{}).Preload("AddressLines").Unscoped().Find(address)
+	b4, _ := json.MarshalIndent(address, "", "  ")
+	fmt.Println(string(b4))
+
 }
